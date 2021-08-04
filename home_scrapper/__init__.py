e234e405f87e86af0a0b8964fc4967abc6033de2
mokanina <mokanina@github.com>
2021-08-04 10:01:46 +0200

    minor reordering & clean up

--- home_scrapper/__init__.py
+++ home_scrapper/__init__.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
 
+from sqlalchemy import create_engine
+
 __version__ = "0.1.0"
 
+# set data directory
 data_dir = Path(__file__).parent.parent / "data"
 if not data_dir.exists():
     data_dir.mkdir(parents=True)
+
+# set DB path
+_db_path = data_dir / "data.db"
+_db_path.unlink(missing_ok=True)  # TODO: this should be handled somehow
+
+# create DB engine
+# db = create_engine('sqlite:///:memory:', echo = True)
+db = create_engine(f"sqlite:///{_db_path}")
--- home_scrapper/results/__init__.py
+++ home_scrapper/results/__init__.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-from sqlalchemy import create_engine
-
 from .homes import Homes
-from home_scrapper import data_dir
+from home_scrapper import db
 
-
-# define DB path and remove if DB exists
-_db_path = data_dir / "data.db"
-_db_path.unlink(missing_ok=True)
-
-# create db engine
-# db = create_engine('sqlite:///:memory:', echo = True)
-db = create_engine(f"sqlite:///{_db_path}")
 Homes.metadata.create_all(db)
