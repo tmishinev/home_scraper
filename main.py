a850e87e22e9a910dbb6014e8a3700e43962a552
mokanina <mokanina@github.com>
2021-08-04 10:01:45 +0200

    main.py cleanup

--- main.py
+++ main.py
@@ -1,34 +1,17 @@
 # -*- coding: utf-8 -*-
 import logging
 
+import pandas as pd
+
+from home_scrapper.results import db
 from home_scrapper.scrapers import ImotScraper
+
 
 logger = logging.getLogger(__name__)
 
 
-def main(url: str, sleep: int = 5):
-    """Runner script for the scrapper!
-
-    :param url: target URL
-    :param sleep: sleep time between URL requests"""
-
-    # run scrapper
-    scraper = ImotScraper(url=url)
-    scraper.run(sleep=sleep)
-
-
-if __name__ == "__main__":
-
-    # Passing the following url to the scraper returns the
-    # search results for the following search criteria:
-    # - sofia
-    # - from 2 rooms to attic
-    # - price <= 130000 EUR
-    # - area>70sqm
-    # - year<=1990
-    # - floor>=2
-    # - bricks
-    # Note that the last number in the url defines the page number (e.g. f1=1 corresponds to the first page).
+def main():
+    """Runner script for the scrapper!"""
 
     # run scrapper on the following search result urls
     urls = {
@@ -38,4 +21,15 @@ if __name__ == "__main__":
     for city, url in urls.items():
         print("=" * 40)
         print(f"Processing data from {city}:")
-        main(url=url)
+        scraper = ImotScraper(url=url)
+        scraper.run(sleep=5)
+
+    # analyse data
+    df = pd.read_sql_query("SELECT * FROM homes;", con=db)
+    print("-" * 40)
+    print(f"Average price: {df.describe().loc['mean', 'price']:.2f} EUR")
+    print(f"Number of properties: {df.shape[0]}")
+
+
+if __name__ == "__main__":
+    main()
